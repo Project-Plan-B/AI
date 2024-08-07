767534dacbf61b9755bed88723ec736a75052b35
Sepro <shtmdwns0@gmail.com>
2024-08-07 21:54:33 +0900

    db 연결

---
+++ Server/database/connection.py
@@ -0,0 +1,9 @@
+data = {
+    'user': 'planb',
+    'password': 'planb!0017',
+    'host': '127.0.0.1',
+    'port': '3307',
+    'database': 'planb'
+}
+
+DB_URL = f"mysql+mysqlconnector://{data['user']}:{data['password']}@{data['host']}:{data['port']}/{data['database']}?charset=utf8"
