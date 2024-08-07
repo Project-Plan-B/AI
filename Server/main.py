076d8c1ac63254c287d4bda3c004e06d1009889d
Sepro <shtmdwns0@gmail.com>
2024-08-07 23:34:42 +0900

    app에 db 연결

--- Server/main.py
+++ Server/main.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from Server.database.model import db
+from Server.database.connection import DB_URL
 from Server.api import other, confusion
 
 app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
+
+db.init_app(app)
 
 app.register_blueprint(confusion.bp)
 app.register_blueprint(other.bp)
