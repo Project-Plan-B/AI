9a1a35e88036cd71e9027855d41b621db30725f9
Sepro <shtmdwns0@gmail.com>
2024-08-10 19:45:29 +0900

    db 연결 + 공지사항 api

--- Server/database/model.py
+++ Server/database/model.py
@@ -1,3 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
+
+
+class Alert(db.Model):
+    __tablename__ = 'alert'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.String(300), nullable=False)
+    writing = db.Column(db.DateTime, default=datetime.now())
--- Server/main.py
+++ Server/main.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from Server.database.model import db
 from Server.database.connection import DB_URL
-from Server.api import other, confusion
+from Server.api import other, confusion, notice
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
+app.config.update(dict(
+    SECRET_KEY="powerful secretkey",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
 
 db.init_app(app)
 
 app.register_blueprint(confusion.bp)
 app.register_blueprint(other.bp)
-
+app.register_blueprint(notice.bp)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
