d6c4888311afab9ba6900148fcf283df9ce46673
Sepro <shtmdwns0@gmail.com>
2024-08-11 11:54:49 +0900

    ♻️ :: error, success 메세지 수정

--- Server/api/notice.py
+++ Server/api/notice.py
@@ -1,7 +1,7 @@
 from Server.database.model import Alert, db
 from flask import Blueprint, jsonify, request
 
-bp = Blueprint('alert', __name__, url_prefix='/list')
+bp = Blueprint('notice', __name__, url_prefix='/list')
 
 
 @bp.route('/create', methods=['GET', 'POST'])
@@ -10,14 +10,14 @@ def add_alert():
         data = request.get_json()
         key = [i for i in data.keys() if i != 'title' and i != 'content']
         if (not data) or key:
-            return {'error': '요청이 잘못되었습니다.'}, 400
+            return {"error": "정보가 잘못되었습니다."}, 400
         a = Alert()
         a.title = data['title']
         a.content = data['content']
         db.session.add(a)
         db.session.commit()
         return jsonify({"success": "공지사항이 만들어졌습니다."}), 201
-    return {'error': "요청이 잘못되었습니다."}, 400
+    return {"error": "요청이 잘못되었습니다."}, 405
 
 
 @bp.route('/cancel/<int:post_id>', methods=["POST"])
@@ -25,8 +25,8 @@ def delete_alert(post_id):
     if request.method == "POST":
         db.session.query(Alert).filter(Alert.id == post_id).delete()
         db.session.commit()
-        return {'success': "성공적으로 삭제되었습니다."}, 204
-    return {'error': "요청이 잘못되었습니다."}, 400
+        return {"success": "성공적으로 삭제되었습니다."}, 204
+    return {"error": "요청이 잘못되었습니다."}, 405
 
 
 @bp.route('/show_all', methods=["GET"])
@@ -34,12 +34,12 @@ def show_alert():
     if request.method == "GET":
         results = db.session.query(Alert).order_by(Alert.id.desc()).all()
         return [{"title": result.title, "writing": result.writing} for result in results], 200
-    return {"error": "요청이 잘못되었습니다."}, 400
+    return {"error": "요청이 잘못되었습니다."}, 405
 
 
 @bp.route('/show/<int:post_id>', methods=["GET"])
 def show_alert_id(post_id):
     if request.method == "GET":
         result = db.session.query(Alert).filter(Alert.id == post_id).first()
-        return result.content, 200
-    return {"error": "요청이 잘못되었습니다."}, 400
+        return {"content": result.content}, 200
+    return {"error": "요청이 잘못되었습니다."}, 405
--- Server/api/other.py
+++ Server/api/other.py
@@ -12,15 +12,14 @@ def info_other():
         data = request.get_json()
         key = [d for d in data.keys() if d != 'temperature' and d != 'humidity']
         if (not data) or key:
-            return {"error": "정보가 잘못되었습니다"}, 400
+            return {"error": "정보가 잘못되었습니다."}, 400
         temperature = data['temperature']
         humidity = data['humidity']
-        return jsonify({'temperature': temperature, 'humidity': humidity}), 201
-    else:
-        return {"error": "오류가 발생했습니다"}, 400
+        return {"success": "성공적으로 정보가 전달되었습니다."}, 201
+    return {"error": "요청이 잘못되었습니다."}, 405
 
 
 # 앱으로 온도, 습도, co2 농도 보내기
-@bp.route('/send', methods=["get"])
+@bp.route('/send', methods=["GET"])
 def send_other():
-    return jsonify({'temperature': temperature, 'humidity': humidity}), 201
+    return jsonify({"temperature": temperature, "humidity": humidity}), 200
