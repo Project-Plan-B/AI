68f5d7ab8fd672d6b50e4c9ad3e38c2bc34278c0
Sepro <shtmdwns0@gmail.com>
2024-08-19 23:35:41 +0900

    ⚡️ ::ai로부터 혼잡도 정보 받기

--- Server/api/confusion.py
+++ Server/api/confusion.py
@@ -1,11 +1,23 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request
 
 bp = Blueprint("confusion", __name__, url_prefix='/confusion')
-confusion = {"A": 2, "B": 3, "C": 1, "D": 2, "E": 4, "F": 3}  # confusion level
+confusion = {"A": 1, "B": 3, "C": 1, "D": 3, "E": 4, "F": 3}  # confusion level
+level = '12233444'
+
+
+# 카메라에서 혼잡도 받기
+@bp.route('/info', methods=["POST"])
+def info_confusion():
+    global confusion
+    if request.method == "POST":
+        data = request.get_json()['list']
+        confu = {k: level[v] for k, v in zip("ABCDEF", data)}
+        confusion = confu
+        return {"message": "혼잡도 정보가 갱신되었습니다."}, 201
+    return {"error": "요청이 잘못되었습니다."}, 405
 
 
 # 앱으로 혼잡도 보내기
-@bp.route('', methods=['GET'])
+@bp.route('/send', methods=['GET'])
 def send_confusion():
-    # ai 이용
-    return jsonify(confusion), 200
+    return confusion, 200
