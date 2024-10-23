66476cfc87ac69987e9dc58bb278d5fde3df81d1
Sepro <shtmdwns0@gmail.com>
2024-10-24 00:21:14 +0900

    ⚙️ ::Flask Server 완성

--- Server/api/alert.py
+++ Server/api/alert.py
@@ -7,14 +7,15 @@ bp = Blueprint('alert', __name__)
 @bp.route('/alert/<int:room_id>', methods=["GET", "POST"])
 def show_alert(room_id):
     if request.method == "POST":
-        want_confu = request.get_json()
+        want_confu = request.get_json()['want_confusion']
         data = Room.query.filter(Room.id == room_id).first()
         if not data:
             return {"error": "해당 데이터가 없습니다."}, 400
         floor = data.floor
         name = data.name
         confusion = data.confusion
-        if confusion <= want_confu:
+        print(confusion, want_confu)
+        if confusion == want_confu:
             return {"message": "현재 자습실이 원하는 혼잡도 상태가 되었습니다."}, 200
         return {"room_floor": floor, "room_name": name, "want_confusion": want_confu}, 201
     return {"error": "요청이 잘못되었습니다."}, 405
--- Server/api/confusion.py
+++ Server/api/confusion.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request
+from Server.database.model import db, Room
 
 bp = Blueprint("confusion", __name__, url_prefix='/confusion')
 confusion = {"A": 1, "B": 2, "C": 1, "D": 3, "E": 4, "F": 3}  # confusion level
-level = '12233444'
+level = '1223344'
 
 
 # 카메라에서 혼잡도 받기
@@ -12,7 +13,11 @@ def info_confusion():
     if request.method == "POST":
         data = request.get_json()['list']
         confu = {k: int(level[v]) for k, v in zip("ABCDEF", data)}
+        # 혼잡도 DB 갱신 필요
         confusion = confu
+        total = sum(data) // 6
+        db.session.query(Room).filter(Room.name == "test").update({"confusion": level[total]})
+        db.session.commit()
         return {"message": "혼잡도 정보가 갱신되었습니다."}, 201
     return {"error": "요청이 잘못되었습니다."}, 405
 
